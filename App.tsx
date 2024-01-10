73d3ce0df343c94d88fa70e9a7be3d121e82846d
Francesco Forte <francesco.forte831tech@gmail.com>
2024-01-09 21:21:35 -0500

    Add typescript hello component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Hello name="Frankie"></Hello>
       <StatusBar style="auto" />
     </View>
   );
